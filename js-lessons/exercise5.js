75397a9294c23f140cf3a1adac1e9b0ff809d5ee
Andy Dobbs <andrew.dobbs@gmail.com>
2023-02-24 08:11:43 -0500

    Add exercise 5 lesson and solution

---
+++ js-lessons/exercise5.js
@@ -0,0 +1,33 @@
+function checkDriverAge (age) {
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+var checkDriverAge2 = function(age) {
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}	
+}
+
+var checkDriverAge3 = function(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
+
+checkDriverAge(18);
+checkDriverAge2(54);
+console.log(checkDriverAge3(9));
