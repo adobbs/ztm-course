a071329979e790f2e35fa15e8904cfa238e7782f
Andy Dobbs <andrew.dobbs@gmail.com>
2023-02-24 17:52:03 -0500

    Add dom exercise solutions

--- js-lessons/domevents/script.js
+++ js-lessons/domevents/script.js
@@ -8,8 +8,18 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(document.createTextNode(input.value + " "));
+	li.addEventListener("click", toggleTodoItem);
 	ul.appendChild(li);
+
+	var button = document.createElement("button");
+	button.appendChild(document.createTextNode("Delete"));
+	button.classList.add("delete");
+	button.addEventListener("click", deleteListItem);
+	// ul.insertBefore(button, li.nextSibling);
+	li.appendChild(button);
+
+	items = ul.children;
 	input.value = "";
 }
 
@@ -28,3 +38,28 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+// Start exercise solutions
+
+// For exercise, need to add event listeners and
+// then update each time a list item is added
+
+var items = Array.from(ul.children);
+
+items.forEach(function (el){
+	el.addEventListener("click", toggleTodoItem);
+});
+
+function toggleTodoItem(e) {
+	e.target.classList.toggle("done");
+}
+
+var deleteButtons = Array.from(document.querySelectorAll(".delete"));
+
+deleteButtons.forEach(function (el){
+	el.addEventListener("click", deleteListItem);
+});
+
+function deleteListItem(e) {
+	e.target.parentElement.remove();
+}
